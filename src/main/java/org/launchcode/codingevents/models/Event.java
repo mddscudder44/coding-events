6cc2c0613fb435349f1c75f5bf38b1c8ab6d51d8
Erin <mddscudder44@gmail.com>
2022-06-29 13:15:08 -0500

    18.4 one-to-one relationship video - completed

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -6,6 +6,8 @@ import java.util.Objects;
  * Created by Chris Bay
  */
 public class Event {
+    private id;
+    private static int nextId = 1;
 
     private String name;
     private String description;
@@ -13,6 +15,12 @@ public class Event {
     public Event(String name, String description) {
         this.name = name;
         this.description = description;
+        this.id = nextId;
+        nextId++;
+    }
+
+    public static int getNextId() {
+        return nextId;
     }
 
     public String getName() {
